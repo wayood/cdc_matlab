16e9c356ac99b412cd51e9834a5ac9ec70bb9a6e
wayood <tomo990203@gmail.com>
2022-06-09 14:50:44 +0900

    初期設定ファイルを追加

---
+++ setup.m
@@ -0,0 +1,11 @@
+mkdir('A_matrix');
+addpath('A_matrix');
+mkdir('evalute');
+addpath('evalute');
+mkdir('path');
+addpath('path');
+mkdir('potential');
+addpath('potential');
+mkdir('wp');
+addpath('wp');
+
